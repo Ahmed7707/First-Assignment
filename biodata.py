902137132d922c1f6a3430206f606c3282d61e1a
Ahmed7707 <ahmedrauf60@hotmail.com>
2020-02-05 18:26:09 +0500

    First file added

---
+++ biodata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+Name= "Ahmed"
+Father_Name="Abdul Rauf"
+Age="22"
+Gender="Male"
+Education="Bcom"
+
+
+# In[11]:
+
+
+print("Name"+":",Name)
+print("Father Name"+":",Father_Name)
+print("Age"+":",Age)
+print("Gender"+":",Gender)
+print("Education"+":",Education)
+
+
+# In[ ]:
+
+
+
+
